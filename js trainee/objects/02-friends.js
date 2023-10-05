30efdf11fa1a0459b86c8e67c80e6848e5e6c0e5
ChewBaccaYeti <chewbaccayeti@gmail.com>
2023-10-05 17:45:12 +0300

    Online/offline friends

--- js trainee/objects/02-friends.js
+++ js trainee/objects/02-friends.js
@@ -7,14 +7,16 @@ const friends = [
     { name: 'Aragorn', online: false },
     { name: 'Arthur', online: false },
 ];
+
 console.table(friends);
 
 for (const friend of friends) {
     // Массив сложных типов, то есть объектов, в цикле итерации forof я получаю ССЫЛКУ на объект
     console.log(friend);
 
-    friend.newProp = 567;
+    friend.newProp = 'This is added property'; // Добавление нового свойства
 }
+
 console.table(friends);
 
 /*
@@ -30,7 +32,6 @@ const findFriendByName = function (allFriends, friendName) {
         }
     }
 };
-
 // !! includes method работает только с коллекцией ПРИМИТИВОВ - числа, строки, були. В массиве он бесполезен
 
 console.log(findFriendByName(friends, 'Arthur'));
@@ -39,3 +40,56 @@ console.log(findFriendByName(friends, 'Aragorn'));
 /*
  * Получаем имена всех друзей
  */
+const getAllNames = function (allFriends) {
+    const arrNames = [];
+
+    for (const friend of allFriends) {
+        console.log(friend.name);
+
+        arrNames.push(friend.name);
+    }
+
+    return arrNames;
+};
+
+console.log(getAllNames(friends));
+
+/*
+ * Получаем имена только друзей которые онлайн
+ */
+const getOnlineFriends = function (allFriends) {
+    const onlineFriends = [];
+
+    for (const friend of allFriends) {
+        console.log(friend);
+        console.log(friend.online);
+
+        if (friend.online) {
+            onlineFriends.push(friends);
+        }
+    }
+
+    return onlineFriends;
+};
+
+console.log(getOnlineFriends(friends));
+
+/*
+ * Получаем имена только друзей которые оффлайн
+ */
+const getOfflineFriends = function (allFriends) {
+    const offlineFriends = [];
+
+    for (const friend of allFriends) {
+        console.log(friend);
+        console.log(friend.online);
+
+        if (!friend.online) {
+            offlineFriends.push(friends);
+        }
+    }
+
+    return offlineFriends;
+};
+
+console.log(getOfflineFriends(friends));
