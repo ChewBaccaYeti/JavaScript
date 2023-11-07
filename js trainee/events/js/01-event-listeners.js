6147cdb012f757b861f9c3fd3dbe150a6e9c7374
ChewBaccaYeti <chewbaccayeti@gmail.com>
2023-11-07 19:23:55 +0200

    events practice preparation

---
+++ js trainee/events/js/01-event-listeners.js
@@ -0,0 +1,16 @@
+/*
+ * События.
+ * - Создание и удаление слушателей
+ * - Именование колбеков для слушателей
+ *    - handle*: handleSubjectEvent
+ *    - *Handler: subjectEventHandler
+ *    - on*: onSubjectEvent
+ * - Ссылочная идентичность колбеков
+ * - Объект события
+ */
+
+const targetBtn = document.querySelector('.js-target-btn');
+
+targetBtn.addEventListener('click', () => {
+    console.log('targetBtn clicked', 'Click!');
+});
