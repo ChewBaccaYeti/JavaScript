01a31fccba48eb07b925d66404c01f4fec72d89d
ChewBaccaYeti <chewbaccayeti@gmail.com>
2023-11-07 20:22:24 +0200

    add event button listener

--- js trainee/events/js/01-event-listeners.js
+++ js trainee/events/js/01-event-listeners.js
@@ -10,7 +10,38 @@
  */
 
 const targetBtn = document.querySelector('.js-target-btn');
+const addListenerButton = document.querySelector('.js-add-listener');
 
+/*
 targetBtn.addEventListener('click', () => {
-    console.log('targetBtn clicked', 'Click!');
+    console.log(
+        'targetBtn clicked',
+        'Click!'
+        // handleTargetButtonClick(),
+        // targetButtonClickHandler(),
+        // onTargetButtonClick(),
+        // logMessage()
+    );
 });
+*/
+
+addListenerButton.addEventListener('click', () => {
+    console.log('Вешаю слушателя событий на целевую кнопку');
+
+    targetBtn.addEventListener('click', () => {
+        console.log('Click!');
+    });
+});
+
+function handleTargetButtonClick() {
+    console.log('handleTargetButtonClick', 'Click');
+}
+function targetButtonClickHandler() {
+    console.log('targetButtonClickHandler', 'Click');
+}
+function onTargetButtonClick() {
+    console.log('onTargetButtonClick', 'Click');
+}
+function logMessage() {
+    console.log('logMessage', 'Click on target button');
+}
