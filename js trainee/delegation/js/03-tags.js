5b69fcb7ff30c6fd4d164a953defab79b780a7a1
ChewBaccaYeti <chewbaccayeti@gmail.com>
2023-11-10 22:02:20 +0200

    targetToggle / targetToggleSet fucntions

--- js trainee/delegation/js/03-tags.js
+++ js trainee/delegation/js/03-tags.js
@@ -6,39 +6,61 @@
 
 const tagsContainer = document.querySelector('.js-tags');
 let selectedTag = null;
-const selectedTags = [];
+const selectedTagsArray = [];
+const selectedTagsSet = new Set();
 
-// tagsContainer.addEventListener('click', onTagsContainerClick);
+tagsContainer.addEventListener('click', onTagsContainerToggleSet);
 
-// function onTagsContainerClick(event) {
-//   if (event.target.nodeName !== 'BUTTON') {
-//     return;
-//   }
+function onTagsContainerClick(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
 
-//   const currentActiveBtn = document.querySelector('.tags__btn--active');
+  const currentActiveBtn = document.querySelector('.tags__btn--active');
 
-//   if (currentActiveBtn) {
-//     currentActiveBtn.classList.remove('tags__btn--active');
-//   }
+  if (currentActiveBtn) {
+    currentActiveBtn.classList.remove('tags__btn--active');
+  }
 
-//   const nextActiveButton = event.target;
-//   nextActiveButton.classList.add('tags__btn--active');
-//   selectedTag = nextActiveButton.dataset.value;
+  const nextActiveButton = event.target;
+  nextActiveButton.classList.add('tags__btn--active');
+  selectedTag = nextActiveButton.dataset.value;
 
-//   console.log(selectedTag);
+  console.log(selectedTag);
 
-//   console.log('event.target', event.target);
-// }
-
-tagsContainer.addEventListener('click', onTagsContainerToggle);
+  console.log('event.target', event.target);
+}
 
 function onTagsContainerToggle(event) {
   if (event.target.nodeName !== 'BUTTON') {
     return;
   }
 
-  event.target.classList.toggle('tags__btn--active');
+  const nextActiveButton = event.target;
+  nextActiveButton.classList.toggle('tags__btn--active');
 
-  selectedTags.push(event.target.dataset.value);
-  console.log(selectedTags);
+  selectedTagsArray.push(nextActiveButton.dataset.value);
+  console.log(nextActiveButton.classList.contains('tags__btn--active'));
+  console.log(selectedTagsArray);
+}
+
+function onTagsContainerToggleSet(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  const btn = event.target;
+  const tag = btn.dataset.value;
+  const isContains = btn.classList.contains('tags__btn--active');
+  const isToggle = btn.classList.toggle('tags__btn--active');
+
+  if (isContains) {
+    selectedTagsSet.delete(tag);
+  } else {
+    selectedTagsSet.add(tag);
+  }
+
+  isToggle;
+
+  console.log(selectedTagsSet);
 }
