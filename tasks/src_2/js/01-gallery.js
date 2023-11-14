bbb7b79f0d27ce5131a8aa252303929716e07d44
ChewBaccaYeti <chewbaccayeti@gmail.com>
2023-11-14 21:39:49 +0200

    1-task lightbox

--- tasks/src_2/js/01-gallery.js
+++ tasks/src_2/js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+  gallery.appendChild(galleryItem);
+});
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+
+  const imageTarget = e.target;
+
+  if (imageTarget.classList.contains('gallery__image')) {
+    const modalWindow = basicLightbox.create(
+      `<div class="modal">
+                <img src='${
+                  galleryItems.find(item => item.preview === imageTarget.src)
+                    .original
+                }' width='1280'></div>`,
+      {
+        onShow: () => {
+          document.addEventListener('keydown', function handler(e) {
+            if (e.code === 'Escape') {
+              modalWindow.close();
+              document.removeEventListener('keydown', handler);
+            }
+          });
+        },
+      },
+    );
+    modalWindow.show();
+  }
+  console.log(e.target, e.currentTarget);
+});
