72c4e031f539373269b614c68736578b144a12f9
ChewBaccaYeti <chewbaccayeti@gmail.com>
2023-11-22 22:34:27 +0200

    localStorage API

--- js_trainee/localStorage/src/js/01-localstorage.js
+++ js_trainee/localStorage/src/js/01-localstorage.js
@@ -53,8 +53,8 @@ try {
 try {
   const data = JSON.parse('Well, this is awkward');
 } catch (error) {
-  console.log(error.name); // "SyntaxError"
-  console.log(error.message); // Unexpected token W in JSON at position 0
+  console.log(error.name);
+  console.log(error.message);
 }
 
 console.log('✅ This is fine, we handled parse error in try...catch');
@@ -62,12 +62,48 @@ console.log('✅ This is fine, we handled parse error in try...catch');
 try {
   const data = JSON.parse('{username: "Mango"}');
 } catch (error) {
-  console.log(error.name); // "SyntaxError"
-  console.log(error.message); // "Unexpected token u in JSON at position 1"
+  console.log(error.name);
+  console.log(error.message);
 }
 
 console.log('✅ This is fine, we handled parse error in try...catch');
 
-// Script will crash during parse
-const data = JSON.parse('Well, this is awkward');
-console.log("❌ You won't see this log");
+localStorage.setItem('ui-theme', 'light');
+localStorage.setItem('sidebar', 'expanded');
+localStorage.setItem('notification-level', 'mute');
+
+localStorage.setItem('ui-theme', 'dark');
+
+const theme = localStorage.getItem('ui-theme');
+console.log(theme);
+
+const settings = {
+  theme: 'dark',
+  isAuthenticated: true,
+  options: [1, 2, 3],
+};
+
+localStorage.setItem('settings', JSON.stringify(settings));
+
+const savedSettings = localStorage.getItem('settings');
+const parsedSettings = JSON.parse(savedSettings);
+console.log(parsedSettings);
+
+localStorage.setItem('ui-theme', 'dark');
+console.log(localStorage.getItem('ui-theme'));
+
+localStorage.removeItem('ui-theme');
+console.log(localStorage.getItem('ui-theme'));
+
+localStorage.setItem('ui-theme', 'light');
+localStorage.setItem('sidebar', 'expanded');
+localStorage.setItem('notification-level', 'mute');
+console.log(localStorage.getItem('ui-theme'));
+console.log(localStorage.getItem('sidebar'));
+console.log(localStorage.getItem('notification-level'));
+
+localStorage.clear();
+
+console.log(localStorage.getItem('ui-theme'));
+console.log(localStorage.getItem('sidebar'));
+console.log(localStorage.getItem('notification-level'));
