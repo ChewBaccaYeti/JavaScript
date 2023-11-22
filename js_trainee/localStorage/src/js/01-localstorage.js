1bde666dce832141826233dc51d74fb98959e33e
ChewBaccaYeti <chewbaccayeti@gmail.com>
2023-11-22 14:32:24 +0200

    tyr/catch, prettier/eslint

--- js_trainee/localStorage/src/js/01-localstorage.js
+++ js_trainee/localStorage/src/js/01-localstorage.js
@@ -18,3 +18,56 @@ const parsedData = JSON.parse(savedData);
 console.log(parsedData);
 
 console.log(localStorage);
+
+try {
+  const agent = {
+    id: '91714cf6-8bc3-4c37-8184-fb3714fade41',
+    tag: 'Green Eye',
+    status: 'active',
+    firstName: 'Mike',
+    lastName: 'Wazowski',
+    department: 'Infilration',
+    corp: 'Monsters Intelligence',
+    role: 'thief',
+  };
+  console.log(agent);
+  console.log(JSON.stringify(agent));
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+  console.log(error.stack);
+}
+
+try {
+  const parsedAgent =
+    '{"id": 91714,"tag": "Green Eye","status": "active","firstName": "Mike","lastName": "Wazowski","department": "Infilration","corp": "Monsters Intelligence","role": "thief"}';
+
+  console.log(parsedAgent);
+  console.log(JSON.parse(parsedAgent));
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+  console.log(error.stack);
+}
+
+try {
+  const data = JSON.parse('Well, this is awkward');
+} catch (error) {
+  console.log(error.name); // "SyntaxError"
+  console.log(error.message); // Unexpected token W in JSON at position 0
+}
+
+console.log('✅ This is fine, we handled parse error in try...catch');
+
+try {
+  const data = JSON.parse('{username: "Mango"}');
+} catch (error) {
+  console.log(error.name); // "SyntaxError"
+  console.log(error.message); // "Unexpected token u in JSON at position 1"
+}
+
+console.log('✅ This is fine, we handled parse error in try...catch');
+
+// Script will crash during parse
+const data = JSON.parse('Well, this is awkward');
+console.log("❌ You won't see this log");
