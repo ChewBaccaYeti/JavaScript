f84f065ae65f3ccb5a5c23f25ad2b572377d73a2
Venedykt Mitishov <chewbaccayeti@gmail.com>
2023-11-24 21:53:26 +0200

    Task 3 completed, junior task added

--- tasks/src_3/src/js/01-gallery.js
+++ tasks/src_3/src/js/01-gallery.js
@@ -1,6 +1,5 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
 import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
@@ -10,49 +9,27 @@ for (const item of galleryItems) {
 
 const gallery = document.querySelector('.gallery');
 
-galleryItems.forEach(item => {
-  const galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+      src="${preview}"
+      data-sources="${original}"
+      alt="${description}" >
+  </a>
+</li>
+`;
+  })
+  .join('');
 
-  const galleryLink = document.createElement('a');
-  galleryLink.classList.add('gallery__link');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
-  const galleryImage = document.createElement('img');
-  galleryImage.classList.add('gallery__image');
-  galleryImage.src = item.preview;
-  galleryImage.alt = item.description;
-
-  galleryLink.appendChild(galleryImage);
-  galleryItem.appendChild(galleryLink);
-  gallery.appendChild(galleryItem);
-});
-
-gallery.addEventListener('click', e => {
-  e.preventDefault();
-
-  const imageTarget = e.target;
-
-  if (imageTarget.classList.contains('gallery__image')) {
-    const modalWindow = basicLightbox.create(
-      `<div class="modal">
-                <img src='${
-                  galleryItems.find(item => item.preview === imageTarget.src)
-                    .original
-                }' width='1280'></div>`,
-      {
-        onShow: () => {
-          document.addEventListener('keydown', function handler(e) {
-            if (e.code === 'Escape') {
-              modalWindow.close();
-              document.removeEventListener('keydown', handler);
-            }
-          });
-        },
-      }
-    );
-    modalWindow.show();
-  }
-  console.log(e.target, e.currentTarget);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
 
 console.log(galleryItems);
--- tasks/src_3/src/js/03-feedback.js
+++ tasks/src_3/src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+let email = '';
+let message = '';
+
+const feedbackRefs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('[name="email"]'),
+  message: document.querySelector('[name="message"]'),
+};
+
+if (localStorage.getItem(STORAGE_KEY)) {
+  const savedFeedback = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  feedbackRefs.email.value = localStorage.getItem(STORAGE_KEY);
+  feedbackRefs.message.value = localStorage.getItem(STORAGE_KEY);
+}
+
+feedbackRefs.form.addEventListener(
+  'input',
+  throttle(e => {
+    if (e.target === feedbackRefs.email) {
+      email = e.target.value;
+    }
+
+    if (e.target === feedbackRefs.message) {
+      message = e.target.value;
+    }
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));
+  }, 500)
+);
+
+feedbackRefs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  console.log({ email, message });
+  email = '';
+  message = '';
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+});
