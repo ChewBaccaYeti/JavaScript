ac6f3aaa52641b4491aec06826495d6fad34d086
Venedykt Mitishov <chewbaccayeti@gmail.com>
2023-12-01 12:40:08 +0200

    promises for example

--- js_trainee/promises/src/js/01-promise-api.js
+++ js_trainee/promises/src/js/01-promise-api.js
@@ -26,3 +26,86 @@ promise.then(
     console.log(error); // reason
   },
 );
+
+// Change value of isSuccess variable to call resolve or reject
+const isSuccess = true;
+
+const promise_ex = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (isSuccess) {
+      resolve('Success! Value passed to resolve function');
+    } else {
+      reject('Error! Error passed to reject function');
+    }
+  }, 2000);
+});
+
+// Will run first
+console.log('Before promise.then()');
+
+// Registering promise callbacks
+promise_ex
+  .then(
+    // onResolve will run third or not at all
+    value => {
+      console.log('onResolve call inside promise.then()');
+      console.log(value); // "Success! Value passed to resolve function"
+    },
+  )
+  // onReject will run third or not at all
+  .catch(reason => {
+    console.log('onReject call inside promise.then()');
+    console.log(reason); // "Error! Error passed to reject function"
+  })
+  .finally(() => console.log('Promise settled')); // "Promise settled"
+
+// Will run second
+console.log('After promise.then()');
+
+// Цепочки промисов
+
+const promise_num = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(5);
+  }, 2000);
+});
+
+promise_num
+  .then(value => {
+    console.log(value); // 5
+    return value * 2;
+  })
+  .then(value => {
+    console.log(value); // 10
+    return value * 3;
+  })
+  .then(value => {
+    console.log(value); // 30
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log('Final task');
+  });
+
+const fetchUserFromServer = username => {
+  return new Promise((resolve, reject) => {
+    console.log(`Fetching data for ${username}`);
+
+    setTimeout(() => {
+      // Change value of isSuccess variable to simulate request status
+      const isSuccess = true;
+
+      if (isSuccess) {
+        resolve('success value');
+      } else {
+        reject('error');
+      }
+    }, 2000);
+  });
+};
+
+fetchUserFromServer('Mango')
+  .then(user => console.log(user))
+  .catch(error => console.error(error));
