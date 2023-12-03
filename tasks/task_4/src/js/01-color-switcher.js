48e16d2ef7d2770476788d63aa125f220f1350e5
Venedykt Mitishov <chewbaccayeti@gmail.com>
2023-12-03 19:00:48 +0200

    color-swithcer with setInterval and click events

--- tasks/task_4/src/js/01-color-switcher.js
+++ tasks/task_4/src/js/01-color-switcher.js
@@ -1,1 +1,46 @@
+const buttons = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+const colorChanger = {
+  interval: null,
+  currentColor: null,
+
+  start() {
+    this.change();
+    this.interval = setInterval(this.change, 1000);
+  },
+
+  stop() {
+    clearInterval(this.interval);
+  },
+
+  change() {
+    do {
+      document.body.style.backgroundColor = getRandomHexColor();
+    } while (this.currentColor === getRandomHexColor());
+
+    this.currentColor = document.body.style.backgroundColor;
+    console.log(
+      `%c ${document.body.style.backgroundColor}`,
+      `color: ${document.body.style.backgroundColor}`
+    );
+  },
+};
+
+buttons.start.addEventListener('click', e => {
+  e.target.disabled = true;
+  colorChanger.start();
+});
+
+buttons.stop.addEventListener('click', e => {
+  buttons.start.disabled = false;
+  colorChanger.stop();
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
