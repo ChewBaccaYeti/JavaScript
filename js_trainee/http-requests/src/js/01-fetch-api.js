9f5e4df20e6cee3ce7b72477358aaae8c1461af6
Venedykt Mitishov <chewbaccayeti@gmail.com>
2023-12-06 19:18:18 +0200

    fetch API and hbs. template, API key and params

--- js_trainee/http-requests/src/js/01-fetch-api.js
+++ js_trainee/http-requests/src/js/01-fetch-api.js
@@ -15,13 +15,12 @@
 
 import '../css/common.css';
 import pokemonCardTpl from '../templates/pokemon-card.hbs';
+import API from './api-service';
+import getRefs from './get-refs';
 
-const refs = {
-  cardContainer: document.querySelector('.js-card-container'),
-  searchForm: document.querySelector('.js-search-form'),
-};
+const refs = getRefs();
 
-refs.searchForm.addEventListener('sumbit', onSearch);
+refs.searchForm.addEventListener('submit', onSearch);
 
 function onSearch(e) {
   e.preventDefault();
@@ -29,26 +28,43 @@ function onSearch(e) {
   const form = e.currentTarget;
   const searchQuery = form.elements.query.value;
 
-  fetchPokemon(searchQuery)
+  API.fetchPokemon(searchQuery)
     .then(renderPokemonCard)
     .catch(onFetchError)
     .finally(() => form.reset());
 }
 
-function onFetchError() {
-  error => {
-    console.dir(error);
-    alert('We did not find your pokemon :(');
-  };
-}
-
-function fetchPokemon(pokemonId) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(
-    response => response.json(),
-  );
+function onFetchError(error) {
+  console.dir(error);
+  alert('We did not find your pokemon :(');
 }
 
 function renderPokemonCard(pokemon) {
   const markup = pokemonCardTpl(pokemon);
   refs.cardContainer.innerHTML = markup;
+}
+
+// =============================================
+
+fetchPokemon();
+
+function fetchPokemon() {
+  const searchPokemonParams = new URLSearchParams({
+    limit: 20,
+    offset: 50,
+  });
+
+  fetch(`https://pokeapi.co/api/v2/pokemon?${searchPokemonParams}`)
+    .then(response => response.json())
+    .then(console.log);
+}
+
+fetchNews();
+
+function fetchNews() {
+  const NEWS_API_KEY = `9f9f313f44e84c20bf98fd54e39e4d8b`;
+
+  fetch(`https://newsapi.org/v2/everything?q=redneck&apiKey=${NEWS_API_KEY}`)
+    .then(response => response.json())
+    .then(console.log);
 }
